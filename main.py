b743ab82f09fb6de0c47b8528f713e441b911290
Gary Arthurs <garypaularthurs@gmail.com>
2025-01-16 17:38:55 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(layout="wide")
+
+df = pd.DataFrame({
+  'Account Name': ["Barclays Bank", "Barclays Saver", "Money Box LISA",
+                   "Money Box Cash ISA","Money Box Simple Saver", "IG Stock & Share ISA",
+                   "IG Stock & Share"],
+  'second column': [10, 20, 30, 40, 4, 6, 7]
+})
+
+
+
+st.title("Networth Tracker")
+
+df
+
+st.dataframe()
+
+st.table()
+
+
